7cc66956c465419461ffd93c215fcac8e9810120
Edoardo Baral <edoardo@baral.it>
2023-02-16 22:20:48 +0100

    Aggiunto http status negli output DTO

--- src/main/java/com/fabrick/banking/dto/CreditTransferOutputDto.java
+++ src/main/java/com/fabrick/banking/dto/CreditTransferOutputDto.java
@@ -3,6 +3,7 @@ package com.fabrick.banking.dto;
 import com.fabrick.banking.model.ErrorModel;
 import lombok.Data;
 import lombok.ToString;
+import org.springframework.http.HttpStatus;
 
 import java.io.Serializable;
 import java.util.List;
@@ -14,6 +15,7 @@ public class CreditTransferOutputDto implements Serializable
 	private static final long serialVersionUID = 1168900495697691201L;
 	
 	private String status;
+	private HttpStatus statusCode;
 	
 	//Vengono dichiarate due liste error ed errors perché nelle API da testare compaiono con nomi differenti, senza uniformità.
 	//Le due liste sono mutuamente esclusive, ne viene popolata solo una per volta
--- src/main/java/com/fabrick/banking/dto/TransactionsListOutputDto.java
+++ src/main/java/com/fabrick/banking/dto/TransactionsListOutputDto.java
@@ -4,6 +4,7 @@ import com.fabrick.banking.model.ErrorModel;
 import com.fabrick.banking.model.TransactionsListPayload;
 import lombok.Data;
 import lombok.ToString;
+import org.springframework.http.HttpStatus;
 
 import java.io.Serializable;
 import java.util.List;
@@ -15,6 +16,7 @@ public class TransactionsListOutputDto implements Serializable
 	private static final long serialVersionUID = -5929486288667648583L;
 	
 	private String status;
+	private HttpStatus statusCode;
 	
 	//Vengono dichiarate due liste error ed errors perché nelle API da testare compaiono con nomi differenti, senza uniformità.
 	//Le due liste sono mutuamente esclusive, ne viene popolata solo una per volta
