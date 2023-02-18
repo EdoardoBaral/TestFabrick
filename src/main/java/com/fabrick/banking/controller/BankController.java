7f20a2c1c052b33518b904e80ac6025f3f083379
Edoardo Baral <edoardo@baral.it>
2023-02-18 22:47:35 +0100

    Aggiunta gestione delle eccezioni nel controller

--- src/main/java/com/fabrick/banking/controller/BankController.java
+++ src/main/java/com/fabrick/banking/controller/BankController.java
@@ -7,9 +7,13 @@ import com.fabrick.banking.interfaces.CreditTransferRequest;
 import com.fabrick.banking.interfaces.CreditTransferResponse;
 import com.fabrick.banking.interfaces.TransactionsListResponse;
 import com.fabrick.banking.service.BankService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 
 @RestController
 @RequestMapping("/api")
@@ -23,8 +27,37 @@ public class BankController
 	@GetMapping(value = "/balance/{accountId}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public BalanceResponse getBalance(@PathVariable(value = "accountId") Long accountId)
 	{
-		BalanceOutputDto outputDto = service.getBalance(accountId);
-		BalanceResponse response = factory.toBalanceResponse(outputDto, accountId);
+		try
+		{
+			BalanceOutputDto outputDto = service.getBalance(accountId);
+			BalanceResponse response = factory.toBalanceResponse(outputDto, accountId);
+			
+			return response;
+		}
+		catch(HttpClientErrorException e)
+		{
+			return manageBalanceResponseError(e);
+		}
+	}
+	
+	private BalanceResponse manageBalanceResponseError(HttpClientErrorException e)
+	{
+		ObjectMapper mapper = new ObjectMapper();
+		HttpStatus statusCode = e.getStatusCode();
+		
+		String jsonResponse = e.getMessage().replace("<EOL>", "");
+		jsonResponse = jsonResponse.substring(jsonResponse.indexOf('{'));
+		
+		BalanceResponse response;
+		try
+		{
+			response = mapper.readValue(jsonResponse, BalanceResponse.class);
+			response.setStatusCode(statusCode);
+		}
+		catch(JsonProcessingException ex)
+		{
+			response = new BalanceResponse();
+		}
 		
 		return response;
 	}
@@ -32,9 +65,38 @@ public class BankController
 	@PostMapping(value = "/creditTransfer", produces = MediaType.APPLICATION_JSON_VALUE)
 	public CreditTransferResponse payCreditTransfer(@RequestBody CreditTransferRequest request)
 	{
-		CreditTransferInputDto inputDto = factory.toCreditTransferInputDto(request);
-		CreditTransferOutputDto outputDto = service.payCreditTransfer(inputDto);
-		CreditTransferResponse response = factory.toCreditTransferResponse(outputDto);
+		try
+		{
+			CreditTransferInputDto inputDto = factory.toCreditTransferInputDto(request);
+			CreditTransferOutputDto outputDto = service.payCreditTransfer(inputDto);
+			CreditTransferResponse response = factory.toCreditTransferResponse(outputDto);
+			
+			return response;
+		}
+		catch(HttpClientErrorException e)
+		{
+			return manageCreditTransferResponseError(e);
+		}
+	}
+	
+	private CreditTransferResponse manageCreditTransferResponseError(HttpClientErrorException e)
+	{
+		ObjectMapper mapper = new ObjectMapper();
+		HttpStatus statusCode = e.getStatusCode();
+		
+		String jsonResponse = e.getMessage().replace("<EOL>", "");
+		jsonResponse = jsonResponse.substring(jsonResponse.indexOf('{'));
+		
+		CreditTransferResponse response;
+		try
+		{
+			response = mapper.readValue(jsonResponse, CreditTransferResponse.class);
+			response.setStatusCode(statusCode);
+		}
+		catch(JsonProcessingException ex)
+		{
+			response = new CreditTransferResponse();
+		}
 		
 		return response;
 	}
@@ -44,9 +106,38 @@ public class BankController
 														@PathVariable(value = "fromAccountingDate") String fromAccountingDate,
 														@PathVariable(value = "toAccountingDate") String toAccountingDate)
 	{
-		TransactionsListInputDto inputDto = factory.toTransactionsListInputDto(accountId, fromAccountingDate, toAccountingDate);
-		TransactionsListOutputDto outputDto = service.getTransactionsList(inputDto);
-		TransactionsListResponse response = factory.toTransactionsListResponse(outputDto);
+		try
+		{
+			TransactionsListInputDto inputDto = factory.toTransactionsListInputDto(accountId, fromAccountingDate, toAccountingDate);
+			TransactionsListOutputDto outputDto = service.getTransactionsList(inputDto);
+			TransactionsListResponse response = factory.toTransactionsListResponse(outputDto);
+			
+			return response;
+		}
+		catch(HttpClientErrorException e)
+		{
+			return manageTransactionsListResponseError(e);
+		}
+	}
+	
+	private TransactionsListResponse manageTransactionsListResponseError(HttpClientErrorException e)
+	{
+		ObjectMapper mapper = new ObjectMapper();
+		HttpStatus statusCode = e.getStatusCode();
+		
+		String jsonResponse = e.getMessage().replace("<EOL>", "");
+		jsonResponse = jsonResponse.substring(jsonResponse.indexOf('{'));
+		
+		TransactionsListResponse response;
+		try
+		{
+			response = mapper.readValue(jsonResponse, TransactionsListResponse.class);
+			response.setStatusCode(statusCode);
+		}
+		catch(JsonProcessingException ex)
+		{
+			response = new TransactionsListResponse();
+		}
 		
 		return response;
 	}
